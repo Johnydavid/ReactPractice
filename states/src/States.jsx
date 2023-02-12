a7d17167c96071a01ae43ebb1256289b514a381e
Johnydavid <dropin2johny@gmail.com>
2023-02-12 18:02:03 +0530

    c

--- states/src/States.jsx
+++ states/src/States.jsx
@@ -1,9 +1,18 @@
-import React from ".react"
+import react, { useState } from "react";
 
-export default function States(){
-    return(
-        <>
-        <h1> Hello World</h1>
-        </>
-    )
+export default function States() {
+  const [value1, setValue1] = useState(0);
+
+  const counting = () => {
+    setValue1(value1 + 1);
+  };
+
+  return (
+    <div>
+      <h1> Hello World</h1>
+      You clicked this button {value1} times
+      <br />
+      <button onClick={counting}> Add </button> <br />
+    </div>
+  );
 }
